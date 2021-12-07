2ab8f801235b8007f155106d214a84385f7a298e
xuruyi136 <364231796@qq.com>
2021-12-07 22:54:45 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to xuruyi Vue.js App"/>
+    <HelloWorld msg="Welcome to xuwei Vue.js App"/>
   </div>
 </template>
 
